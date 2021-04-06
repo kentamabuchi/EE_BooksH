6c16acb4c081839e18353f099200bf3cb21848f8
EC2 Default User <ec2-user@ip-172-31-14-169.ap-northeast-1.compute.internal>
2021-04-06 09:17:02 +0000

    add user homes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   root to: 'public/homes#top'
   
   scope module: 'public' do
-    get '/' => 'public/homes#top'
-    get 'about' => 'public/homes#about'
+    get '/' => 'homes#top'
+    get '/about' => 'homes#about'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
