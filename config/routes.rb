7ae357e3fcf0db6402b6f6054c5ce7b2449957a9
EC2 Default User <ec2-user@ip-172-31-14-169.ap-northeast-1.compute.internal>
2021-04-06 14:18:20 +0000

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,35 @@
 Rails.application.routes.draw do
   
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :admin do
+    get 'genre/index'
+  end
+  namespace :admin do
+    get 'classifications/index'
+  end
+  namespace :admin do
+    get 'books/index'
+    get 'books/new'
+    get 'books/show'
+    get 'books/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'books/index'
+    get 'books/show'
+    get 'books/edit'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+    get 'users/status'
+  end
   devise_for :admins
   devise_for :users
   root to: 'public/homes#top'
