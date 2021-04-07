8ff6ae45e48f09386a66a179acd5fefb28c39135
EC2 Default User <ec2-user@ip-172-31-14-169.ap-northeast-1.compute.internal>
2021-04-07 03:58:05 +0000

    add user logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+    
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    protected
+    
+    def configure_permitted_parameters
+        if resource_class == User
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name,
+                                                               :email,
+                                                               :created_at,
+                                                               :updated_at])
+            devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+            devise_parameter_sanitizer.permit(:account_ipdate, keys: [:nick_name,
+                                                                      :email,
+                                                                      :created_at,
+                                                                      :updated_at])
+        elsif resource_class == Admin
+            devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+            devise_parameter_sanitizer.permit(:account_ipdate, keys: [:name,
+                                                                      :email,
+                                                                      :created_at,
+                                                                      :updated_at])
+        end
+    end
+    
 end
