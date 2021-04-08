e2446e9f22cdc7554d252341666511bdc3bc5322
EC2 Default User <ec2-user@ip-172-31-14-169.ap-northeast-1.compute.internal>
2021-04-08 01:55:13 +0000

    add admin classification

--- app/controllers/admin/classifications_controller.rb
+++ app/controllers/admin/classifications_controller.rb
@@ -1,4 +1,31 @@
 class Admin::ClassificationsController < ApplicationController
+  
   def index
+    @classification = Classification.new
+    @classifications = Classification.all
   end
+  
+  def create
+    @classification = Classification.new(classification_params)
+    @classification.save(classification_params)
+    redirect_to admin_classifications_path
+  end
+  
+  def update
+    @classification = Classification.find(params[:id])
+    @classification.update(classification_params)
+    redirect_to admin_classifications_path
+  end
+  
+  def destroy
+    @classification = Classification.find(params[:id])
+    @classification.destroy
+    redirect_to admin_classifications_path
+  end
+  
+  def classification_params
+    params.require(:classification).permit(:name, :created_at, :updated_at)
+  end
+  
 end
+
